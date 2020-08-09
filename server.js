070d3f6a0b7446eee1da0462673d6f8149e163e1
FalconZ <animj.emma@gmail.com>
2020-08-09 21:17:13 +0100

    app set up to recieve json data type

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log("Server is running on port: ${port}");
