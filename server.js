8c9bda92b16c2edcfc56aaf7e7a1dacd490f1f32
FalconZ <animj.emma@gmail.com>
2020-08-10 18:33:20 +0100

    unifiedTopology set to true in mongoose uri connection

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 
 app.listen(port, () => {
-    console.log("Server is running on port: ${port}");
+    console.log("Server is running on port:", port);
 });
