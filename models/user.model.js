053623cd395751c13e73d619a6dd614ee2b010ae
FalconZ <animj.emma@gmail.com>
2020-08-10 20:16:46 +0100

    user schema attribute username set to required and unique

--- models/user.model.js
+++ models/user.model.js
@@ -7,6 +7,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {
         type: String,
+        required: true,
+        unique: true,
     },
 }, {
     timestamps: true,
