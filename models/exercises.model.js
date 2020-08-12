efd642c1753d16ae2024f4136df02c9d9108caf7
FalconZ <animj.emma@gmail.com>
2020-08-12 21:13:09 +0100

    exerciseSchema username field made required

--- models/exercises.model.js
+++ models/exercises.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-    username: { type: String },
+    username: { type: String, required: true },
     description: {},
     duration: {},
     date: {},
