86bf639f660ff664e7e3d94c197d0ecaaf21877a
FalconZ <animj.emma@gmail.com>
2020-08-12 21:12:20 +0100

    exerciseSchema username field set to type String

--- models/exercises.model.js
+++ models/exercises.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const exerciseShema = new Schema({
-    username: {},
+const exerciseSchema = new Schema({
+    username: { type: String },
     description: {},
     duration: {},
     date: {},
