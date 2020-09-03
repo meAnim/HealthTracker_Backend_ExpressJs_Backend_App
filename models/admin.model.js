395fe44e31c4612ac28c09324a4d49c65466818d
FalconZ <animj.emma@gmail.com>
2020-09-03 20:39:45 +0100

    admin schema created

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,1 +1,9 @@
 const mongoose = require("mongoose");
+const adminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
