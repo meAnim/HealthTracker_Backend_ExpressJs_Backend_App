c94eb4268b5d669b6254230bc2914a6bbfdf5cc3
FalconZ <animj.emma@gmail.com>
2020-09-03 20:44:03 +0100

    admin username attribute made unique

--- models/admin.model.js
+++ models/admin.model.js
@@ -3,6 +3,9 @@ const adminSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
     },
 }, {
     timestamps: true,
